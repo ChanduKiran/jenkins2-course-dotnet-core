0aa1d832933fe6bf693664a48af15756e615298d
Wes Demos <wesdemos@lappy.local>
2016-07-25 18:06:35 -0500

    Adding some fake testing for CI purposes

---
+++ samples/qotd/Tests.cs
@@ -0,0 +1,19 @@
+using Xunit;
+
+namespace Qotd
+{
+    public class QuoteTests
+    {
+        [Fact]
+        public void CanReadQuoteFile()
+        {
+            Assert.True(true);
+        }
+
+        [Fact]
+        public void CanPrintQuote()
+        {
+            Assert.True(true);
+        }
+    }
+}
